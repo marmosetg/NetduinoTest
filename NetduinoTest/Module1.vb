d4575575dfae0d6ee28ac543b93a01be8fcbe919
Scott <github@stickpig.com>
2015-04-10 06:39:46 -0400

    Init Commit

---
+++ NetduinoTest/Module1.vb
@@ -0,0 +1,14 @@
+Imports Microsoft.SPOT
+Imports Microsoft.SPOT.Hardware
+Imports SecretLabs.NETMF.Hardware
+Imports SecretLabs.NETMF.Hardware.Netduino
+
+Module Module1
+
+    Sub Main()
+        ' write your code here
+
+
+    End Sub
+
+End Module
